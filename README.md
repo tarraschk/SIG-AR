c39b2eb986b5a3c61623f71a40e93ae3262d0200
Maxime ALAY-EDDINE <maxime.alayeddine@gmail.com>
2012-09-21 16:37:47 +0200

    Création d'un projet Android SIG-AR

--- README.md
+++
@@ -1,6 +0,0 @@
-SIG-AR
-======
-
-Augmented-Reality applied to the buildings of Nantes (France), using the Mobile AR Framework and an Android tablet.
-
-Project developed by the Computer Science class at the Ecole Centrale de Nantes.
---
+++ src/com/ecn/ei3info/sig_ar/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ecn.ei3info.sig_ar;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}

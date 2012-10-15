215c6144707b49ce8e34d4cdb095b7c4152add6c
BMRagot <bastien.marichal@gmail.com>
2012-10-15 08:56:53 +0200

    Add folders for 3DModels

--- src/com/ecn/ei3info/sig_ar/DataManager.java
+++ src/com/ecn/ei3info/sig_ar/DataManager.java
@@ -4,6 +4,7 @@ import com.hitlabnz.outdoorar.data.OADataManager;
 
 public class DataManager extends OADataManager{
 	
+	private static final String DEFAULT_WORKING_PATH = "SIGAR";
 	
 	//TODO add folder to model management 
 	//TODO Generate Xml file

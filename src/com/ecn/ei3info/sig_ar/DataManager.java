34b520e3a661d1a22f03e368e8502fcd5096b283
BMRagot <bastien.marichal@gmail.com>
2012-10-15 08:37:13 +0200

    Update Task and create DataManage

---
+++ src/com/ecn/ei3info/sig_ar/DataManager.java
@@ -0,0 +1,10 @@
+package com.ecn.ei3info.sig_ar;
+
+import com.hitlabnz.outdoorar.data.OADataManager;
+
+public class DataManager extends OADataManager{
+	
+	
+	//TODO add folder to model management 
+	//TODO Generate Xml file
+}
---
+++ src/com/ecn/ei3info/sig_ar/Scene.java
@@ -0,0 +1,18 @@
+package com.ecn.ei3info.sig_ar;
+
+import com.hitlabnz.outdoorar.api.OAScene;
+
+public class Scene extends OAScene{
+	
+	public String region;
+
+	//TODO ajouter les informations supplémentaires des scènes.
+	
+	public Scene(){
+		//TODO completer les constructeurs de scene
+		
+	}
+	
+	
+	
+}

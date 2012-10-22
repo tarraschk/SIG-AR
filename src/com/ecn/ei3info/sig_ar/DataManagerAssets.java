6e81c0c89a38e4a9d7f04e961d835846fc61b71e
BMRagot <bastien.marichal@gmail.com>
2012-10-22 17:06:09 +0200

    add dataManagerAssets

---
+++ src/com/ecn/ei3info/sig_ar/DataManagerAssets.java
@@ -0,0 +1,54 @@
+package com.ecn.ei3info.sig_ar;
+
+import java.util.List;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.hitlabnz.androidar.data.SceneData;
+import com.hitlabnz.outdoorar.api.OAScene;
+import com.hitlabnz.outdoorar.data.OADataManagerAssets;
+
+public class DataManagerAssets extends OADataManagerAssets{
+
+	private static DataManagerAssets instance = null;
+
+	
+	public DataManagerAssets(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public DataManagerAssets(String workingPath, Context context){
+		super(workingPath,context);
+	}
+	
+	public static DataManagerAssets getInstance(Context context) {
+		if (instance == null){
+			instance = new DataManagerAssets("SIGAR",context);
+		}
+		Log.w("myApp", Integer.toString(instance.getSceneCount()));
+		return instance;
+	}
+	
+	
+	public int getSceneCount(){
+		return this.scenes.size();
+	}
+	
+	/**
+	 * Called back when the library is ready for data manager to load up (or add) the scenes.
+	 * Populates the data manager by adding scenes. 
+	 */
+	/*@Override
+	protected void loadScenes() {
+		if(getDataLoader() != null) {
+			List<SceneData> scenes = getDataLoader().getSceneData();
+			for(SceneData scene: scenes)
+				if (scene.id){
+					addScene(new OAScene(scene));
+				}
+		}
+	}*/
+	
+}

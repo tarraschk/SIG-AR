83b1ea6fb014624429f96166384206c3f629c917
Aurore <aurore.lemoigne@gmail.com>
2012-12-06 21:31:09 +0100

    Settings

--- src/com/ecn/ei3info/sig_ar/SettingsActivity.java
+++ src/com/ecn/ei3info/sig_ar/SettingsActivity.java
@@ -3,8 +3,12 @@ package com.ecn.ei3info.sig_ar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+
 
 public class SettingsActivity extends Activity {
+	
 	    @Override
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
@@ -15,7 +19,7 @@ public class SettingsActivity extends Activity {
 	                .commit();
 	    }
 	    
-	    public static class SettingsFragment extends PreferenceFragment {
+	    public static class SettingsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
 	        @Override
 	        public void onCreate(Bundle savedInstanceState) {
 	            super.onCreate(savedInstanceState);
@@ -23,6 +27,27 @@ public class SettingsActivity extends Activity {
 	            // Load the preferences from an XML resource
 	            addPreferencesFromResource(R.xml.preferences);
 	        }
+	        
+		    @Override
+			public void onResume() {
+		        super.onResume();
+		        getPreferenceManager().getSharedPreferences()
+		                .registerOnSharedPreferenceChangeListener(this);
+		    }
+
+		    @Override
+			public void onPause() {
+		        super.onPause();
+		        getPreferenceManager().getSharedPreferences()
+		                .unregisterOnSharedPreferenceChangeListener(this);
+		    }
+
+			@Override
+			public void onSharedPreferenceChanged(
+					SharedPreferences sharedPreferences, String key) {
+				// TODO Auto-generated method stub
+				
+			}
 	    }
 }
     
